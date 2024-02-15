697d51954898c83e4e510cbed3085d3aa1448814
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-02-15 07:54:32 +0100

    Fixed fetch logic, and added some display logic

--- data.js
+++ data.js
@@ -1,20 +1,23 @@
-let obj;
-
 const apiUrl =
   "https://data.goteborg.se/RiverService/v1.1/measuresites/2b1ac402-554c-422d-b481-0410ad748a55?format=json";
 
-/* fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    obj = data;
-  });
- */
 let outsideVariable;
 
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    outsideVariable = data;
-  });
+ 
+/* async function fetchData() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+}
+outsideVariable = await fetchData();
+ */
+
+function fetchData() {
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {return data;})    
+}
+
+outsideVariable = await fetchData();
 
 export { outsideVariable };
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 import { outsideVariable } from "/data.js";
+
+let stationList = document.getElementById('station-list');
+
 const stations = outsideVariable;
 console.log(stations);
+
+//istället för denna loop så får vi hårdkoda varje stationsnamn, men ville bara testa å få fram all data först
+stations.forEach(station => {
+  let listItem = document.createElement('li');
+  listItem.textContent = station.Code; 
+  stationList.appendChild(listItem);
+});
+//så tex: 
+let agnesberg = stations[0].Code;
+console.log(agnesberg);
